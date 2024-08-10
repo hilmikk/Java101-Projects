4190f2b06add34613df929f354c025638fc76132
[Erdem Kaya] <[he.kaya1140@gmail.com]>
2024-08-10 13:17:54 +0300

    VAT with variables

---
+++ VATwithVariables.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class VATwithVariables {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double price;
+        System.out.println("Price for product:");
+        price= input.nextDouble();
+
+        Double vatrate = (price<1000) ? 0.18:0.08 ;
+
+        
+
+        double vat= vatrate*price;
+        double total= vat + price;
+        
+        System.out.println("Price: " + price);
+        System.out.println("VAT Rate: " + vatrate);
+        System.out.println("VAT: " + vat);
+        System.out.println("Total: " + total);
+
+        
+
+
+        
+        
+    }
+    
+}
