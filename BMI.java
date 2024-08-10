f899d282425542bc508a139f039e7535f2aae389
[Erdem Kaya] <[he.kaya1140@gmail.com]>
2024-08-10 14:08:15 +0300

    bmi

---
+++ BMI.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double height,weight,bmi;
+
+
+        System.out.println("Your height as m: ");
+        height = input.nextDouble();
+        System.out.println("Your weight as kg: ");
+        weight = input.nextDouble();
+
+        bmi=weight/height/height;
+        System.out.println("Your BMI: " + bmi);
+
+
+    }
+    
+}
