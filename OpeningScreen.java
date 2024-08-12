8843e2cd0e960a8170551b00d4f9786fda762bbe
[Erdem Kaya] <[he.kaya1140@gmail.com]>
2024-08-12 15:03:58 +0300

    facebookEntry

---
+++ OpeningScreen.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OpeningScreen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Username: ");
+        String username = input.nextLine();
+        System.out.println("Password");
+        String password = input.nextLine();
+
+
+        if  ((username.equals("ardahan")) && (password.equals("4141")))
+        {
+            System.out.println("Welcome to Facebook");
+        }
+        else {
+            System.out.println("Entry denied");
+        }
+    }
+    
+}
