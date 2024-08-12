35be412be6a365aadb69c3bf4c600c84183b5d1a
[Erdem Kaya] <[he.kaya1140@gmail.com]>
2024-08-12 17:23:14 +0300

    least to greastest

--- LeastToGreatest.java
+++ LeastToGreatest.java
@@ -2,45 +2,48 @@ import java.util.Scanner;
 
 public class LeastToGreatest {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a,b,c;
-        System.out.println("1st number: ");
-        a=in.nextInt();
-        System.out.println("2nd number: ");
-        b=in.nextInt();
-        System.out.println("3rd number: ");
-        c=in.nextInt();
+        try (Scanner in = new Scanner(System.in)) {
+            int a,b,c;
+            System.out.println("1st number: ");
+            a=in.nextInt();
+            System.out.println("2nd number: ");
+            b=in.nextInt();
+            System.out.println("3rd number: ");
+            c=in.nextInt();
 
-        System.out.println("Your numbers are\n" + "(a)" + a+ "\t(b)" + b+ "\t(c)" + c);
+            System.out.println("""
+                               Your numbers are
+                               (a)""" + a+ "\t(b)" + b+ "\t(c)" + c);
 
-        if((a==b)||(b==c)||(a==c)){
-            System.out.println("Write non-equal numbers");
-        }
-        else {
-        if((a<b) && (a<c)){
-            if(b<c){
-                System.out.println("a<b<c");
-            }
-            else{
-                System.out.println("a<c<b");
-            }
-        }
-        else if((b<c) && (b<a)){
-            if(c<a){
-                System.out.println("b<c<a");
+            if((a==b)||(b==c)||(a==c)){
+                System.out.println("Write non-equal numbers");
             }
             else {
-                System.out.println("b<a<c");
+            if((a<b) && (a<c)){
+                if(b<c){
+                    System.out.println("a<b<c");
+                }
+                else{
+                    System.out.println("a<c<b");
+                }
             }
-        }
-        else{
-            if(b<a){
-                System.out.println("c<b<a");
+            else if((b<c) && (b<a)){
+                if(c<a){
+                    System.out.println("b<c<a");
+                }
+                else {
+                    System.out.println("b<a<c");
+                }
             }
             else{
-                System.out.println("c<a<b");
+                if(b<a){
+                    System.out.println("c<b<a");
+                }
+                else{
+                    System.out.println("c<a<b");
+                }
             }
-        }
+            }
         }
         
         
