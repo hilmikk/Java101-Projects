e60bc91c41e0c722a657c40e5076e96c3568bb1e
[Erdem Kaya] <[he.kaya1140@gmail.com]>
2024-08-13 18:30:03 +0300

    harmonic

--- HarmonicNumber.java
+++ HarmonicNumber.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class HarmonicNumber {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Your number: ");
-        int a = in.nextInt();
-        double sum = 0;
-        
-        for(double i = 1 ; i<=a; i++){
-            sum +=(1/i);
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Your number: ");
+            int a = in.nextInt();
+            double sum = 0;
+            
+            for(double i = 1 ; i<=a; i++){
+                sum +=(1/i);
+            }
+            System.out.println("Harmonic series of " + a +" is: " + sum);
         }
-        System.out.println("Harmonic series of " + a +" is: " + sum);
     }
     
 }
