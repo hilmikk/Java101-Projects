2b56eaf088110c453d83af1d26c9fcfba6ffd24f
[Erdem Kaya] <[he.kaya1140@gmail.com]>
2024-08-15 02:15:38 +0300

    prime number

--- PrimeNumberRecursive.java
+++ PrimeNumberRecursive.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner;   
 public class PrimeNumberRecursive {
     static int isPri (int num, int i){
         if(num==i){
@@ -15,7 +15,7 @@ public class PrimeNumberRecursive {
 
     }
     public static void main(String[] args) {
-        
+
         Scanner in = new Scanner(System.in);
         System.out.println("Your number: ");
         int a = in.nextInt();
